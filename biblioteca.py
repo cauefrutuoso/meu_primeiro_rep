5d10f4dda78d95605c2ee8bf9e4dbe3c94174925
dev-igor1 <senaiigor096@gmail.com>
2024-10-18 16:59:40 -0300

    biblioteca

---
+++ biblioteca.py
@@ -0,0 +1,19 @@
+class Celular:
+    
+    marca = 'Nokia'
+    modelo = 'Tijolão'
+    cor = 'Azul'
+    tem_camera = False
+    bateria = 'Infinita'
+
+    def fazer_ligações(self):
+        print('Fazendo ligação...')
+
+    def jpgar_cobrinha(self):
+        print('Jogando cobrinha')
+    
+    def despertar(self):
+        print('Despertando')
+
+celular = Celular()
+celular.bateria
